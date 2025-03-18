dc2bac2445764c724463313860e5956102ebc2f5
Edson André <andreedson02@gmail.com>
2025-03-18 00:59:16 -0300

    initial commit

---
+++ scripts/cadastro.js
@@ -0,0 +1,23 @@
+document
+  .getElementById('formContato')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const contato = {
+      nome: document.getElementById('nome').value,
+      fone: document.getElementById('disciplina').value,
+      email: document.getElementById('nota').value,
+    };
+
+    fetch('URL - api', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(contato),
+    })
+      .then((response) => response.json())
+      .then(() => {
+        alert('Contato cadastrado com sucesso!');
+        window.location.href = 'contatos.html';
+      })
+      .catch((error) => console.error('Erro ao cadastrar contato:', error));
+  });
